2b42d7ac423ad0198556746e3f789d094b541e0b
Craig McLeod <mail@camcleod.com>
2025-03-05 15:37:35 +0000

    v 1.0.0 - Gui Setup Done and copy function working

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
 import re
 import tkinter as tk
+from tkinter import Tk, Canvas, Label, PhotoImage
+from tkmacosx import Button
+from turtledemo.nim import COLOR
+
 from season import *
 import pyperclip
+import calendar
+
+COLOR_BACKGROUND = '#B1DDC6'
 
 if __name__ == '__main__':
+    # Setup
     season = get_season()
     now = get_now()
-    print(f"{now.year}-{'{:0>2}'.format(now.month)}-{'{:0>2}'.format(now.day)}")
-    print(f"{now.strftime("%a")}")
-    print(f"This is day {'{:0>3}'.format(now.timetuple().tm_yday)} of 2025")
-    print(f"This is day {(season[0])} of {season[2]}")
-    print(f"There is {(season[1])} days left of {season[2]}")
+    year_days = 366 if calendar.isleap(now.year) else 365
 
-    journal_string = f"📅️ {now.strftime("%a")} | 🌏️ {'{:0>3}'.format(now.timetuple().tm_yday)} / 365 | 🌲️ {season[0]} / {season[0] + season [1]}"
-    pyperclip.copy(journal_string)
-    print(f"📅️ {now.strftime("%a")} | 🌏️ {'{:0>3}'.format(now.timetuple().tm_yday)} / 365 | 🌲️ {season[0]} / {season[0] + season [1]}")
+    string_date = f"{now.year}-{'{:0>2}'.format(now.month)}-{'{:0>2}'.format(now.day)}"
+    string_journal = f"📅️ {now.strftime("%a")} | 🌏️ {'{:0>3}'.format(now.timetuple().tm_yday)} / {year_days} | {season[2]} {season[0]} / {season[0] + season[1]}"
+
+    # Functions
+    def copy_date():
+        pyperclip.copy(string_date)
+        return None
+
+    def copy_journal():
+        pyperclip.copy(string_journal)
+        return None
+
+    # Screen
+    win = Tk()
+    win.title("Journalist's Barometer")
+    win.config(padx=50, pady=50, bg=COLOR_BACKGROUND)
+
+    canvas = Canvas(height=800, width=800)
+    canvas.config(bg=COLOR_BACKGROUND, highlightthickness=0)
+
+    # Labels
+    label_email = Label(text="Date", padx=5, width=10)
+    label_email.grid(column=1, row=1)
+
+    label_website = Label(text="Journal String", padx=5, width=10)
+    label_website.config(padx=5)
+    label_website.grid(column=1, row=2)
+
+    # Text Fields
+    label_date = Label(text=string_date, padx=5, pady=5, width=50, background=COLOR_BACKGROUND)
+    label_date.grid(column=2, row=1)
+
+    label_journal = Label(text=string_journal, padx=5, pady=5, width=50, background=COLOR_BACKGROUND)
+    label_journal.config(padx=5)
+    label_journal.grid(column=2, row=2)
+
+    # Buttons
+    button_find_password = Button(text="📋️", command=copy_date,borderless=1, highlightthickness=0, background="grey")
+    button_find_password.grid(column=3, row=1)
+
+    button_save_email = Button(text="📋️", command=copy_journal,borderless=1, highlightthickness=0,  background="grey")
+    button_save_email.grid(column=3, row=2)
+
+    win.mainloop()
--- season.py
+++ season.py
@@ -2,6 +2,11 @@ import datetime as dt
 
 dt_now = dt.datetime.now()
 
+STRING_WINTER = "❄️️"
+STRING_SPRING = "🌲️"
+STRING_SUMMER = "☀️️"
+STRING_AUTUMN = "🍂️"
+
 def get_now():
     return dt_now
 
@@ -13,8 +18,8 @@ def get_season():
     dt_last_winter = dt.datetime(dt_now.year - 1,12,21)
     dt_next_spring = dt.datetime((dt_now.year + 1),3, 21)
 
-    if dt_now < dt_spring:      return (dt_now - dt_last_winter).days, (dt_spring - dt_now).days,  "Winter"
-    elif dt_now < dt_summer:    return (dt_winter - dt_now).days, (dt_summer - dt_now).days,  "Spring"
-    elif dt_now < dt_autumn:    return (dt_spring - dt_now).days, (dt_autumn - dt_now).days,  "Summer"
-    elif dt_now < dt_winter:    return (dt_summer - dt_now).days, (dt_winter - dt_now).days,  "Autumn"
-    else:                       return (dt_autumn - dt_now).days, (dt_next_spring - dt_now).days,  "Winter"
+    if dt_now < dt_spring:      return (dt_now - dt_last_winter).days, (dt_spring - dt_now).days, STRING_WINTER
+    elif dt_now < dt_summer:    return (dt_winter - dt_now).days, (dt_summer - dt_now).days, STRING_SPRING
+    elif dt_now < dt_autumn:    return (dt_spring - dt_now).days, (dt_autumn - dt_now).days, STRING_SUMMER
+    elif dt_now < dt_winter:    return (dt_summer - dt_now).days, (dt_winter - dt_now).days, STRING_AUTUMN
+    else:                       return (dt_autumn - dt_now).days, (dt_next_spring - dt_now).days, STRING_WINTER
